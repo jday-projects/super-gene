47c3300053e82189b8d078712467d053e2c00dd5
jday-projects <71560762+jday-projects@users.noreply.github.com>
2023-04-02 14:27:18 -0400

    Add a function for getting the fitness of a high level permutation, and one to make a similarity matrix to count overlap of elements (used in the fitness function)

--- Superpermutations.jl
+++ Superpermutations.jl
@@ -2,20 +2,41 @@
 module Superpermutations
 
 export overlap
+export generate_similarity_matrix
+export high_perm_fitness
 
 # Note: overlap(a, b) =/= overlap(b, a)
-function overlap(a, b)
+function overlap(low_perm1, low_perm2)
 
-    for i in 1:length(a)
-        if a[(end-i+1):end] == b[begin:(begin+i-1)]
+    for i in 1:length(low_perm1)
+        if low_perm1[(end-i+1):end] == low_perm2[begin:(begin+i-1)]
             overlap_amount = i
             return(overlap_amount)
         end
     end
 
+    #Through loow with NO OVERLAP
     return(0)
 end
 
-function
+function generate_similarity_matrix(n)
+
+    similarity_matrix = [overlap(i,j) for i in permutations(1:n), j in permutations(1:n)]
+
+    return(similarity_matrix)
+
+end
+
+function high_perm_fitness(high_perm, n)
+
+    total_overlap = 0
+    similarity_matrix = generate_similarity_matrix(n)
+    for i in 2:length(high_perm)
+        global total_overlap += similarity_matrix[high_perm[i-1], high_perm[i]]
+    end
+
+    return(total_overlap)
+
+end
 
 end
