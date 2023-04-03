fb0823a120ed215b0953fac531adb5f41aba85d7
jday-projects <71560762+jday-projects@users.noreply.github.com>
2023-04-03 11:34:17 -0400

    Fix a bunch of errors and issues - this is the first version that actually works and does something.

--- main.jl
+++ main.jl
@@ -24,5 +24,52 @@
 # But for now I don't care. 
 #
 
+include("Superpermutations.jl")
+include("Mutations1.jl")
+
+using .Superpermutations
+using .Mutations1
+using StatsBase
+using Combinatorics
+
+object_count = 4 # number of elements in low permutation
+high_perm_length = factorial(object_count)
+generation_count = 500
+best_values = zeros(generation_count)
+population_size = 10
+elite_count = 2
+similarity_matrix = generate_similarity_matrix(object_count)
+population = [sample(1:high_perm_length, high_perm_length, replace=false) for i in 1:population_size]
+
+for i in 1:generation_count
+
+    global population
+    global best_values
+
+    #Calculate fitness
+    fitness_vals = [high_perm_fitness(i, similarity_matrix) for i in population]
+    #Sort based on fitness
+    population = population[reverse(sortperm(fitness_vals))]
+
+    #Save off best
+    best_values[i] = maximum(fitness_vals)
+    
+    #Take the top contenders
+    elite_members = copy(population[1:elite_count])
+    
+    population = copy(elite_members)
+
+    #Mutate to fill out new population
+    for j in (elite_count+1):population_size
+        global population
+        #global elite_members
+        push!(population, mutate(sample(elite_members, 1)[1]))
+    end
+
+end
+
+#print(population)
+using Plots
+plot(best_values)
 
 
